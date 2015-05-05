137d83776ece9d1722d9b32834ee5fc6579f241d
katiepru <katiepru@nbcs.rutgers.edu>
2015-05-04 21:09:47 -0400

    adding some google maps api interation

---
+++ ru_rideshare/static/js/rideshare.js
@@ -0,0 +1,38 @@
+/* Google Maps API Interaction */
+
+var map = null;
+var geocoder = null;
+var marker = null;
+
+function initialize() {
+        var mapOptions = {
+            center: { lat: 40.498818, lng: -74.441862},
+            zoom: 12
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);
+    geocoder = new google.maps.Geocoder();
+}
+
+function showAddress(address) {
+  if (geocoder) {
+    geocoder.geocode(
+      {address: address},
+      function(points) {
+        if (!points) {
+          alert(address + " not found");
+        } else {
+          latlng = {lat: points[0].geometry.location.lat(), lng: points[0].geometry.location.lng()}
+          map.setCenter(latlng, 12);
+          marker = new google.maps.Marker({
+              position: latlng,
+              map: map,
+              title: address,
+              animation: google.maps.Animation.DROP,
+              draggable: true
+          });
+        }
+      }
+    );
+  }
+}
